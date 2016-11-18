af7bc81a44bf704a95e5ef6668d63cbd511039fb
zdz <437773935@qq.com>
2016-11-18 16:43:46 +0800

    format

--- mips1-1.asm
+++ mips1-1.asm
@@ -31,133 +31,72 @@
     end:    .asciiz "?"
     failed: .asciiz "*\n"
 
-    _A: .asciiz "Alpha\n"
-    _a: .asciiz "alpha\n"
-    _B: .asciiz "Bravo\n"
-    _b: .asciiz "bravo\n"
-    _C: .asciiz "China\n"
-    _c: .asciiz "china\n"
-    _D: .asciiz "Delta\n"
-    _d: .asciiz "delta\n"
-    _E: .asciiz "Echo\n"
-    _e: .asciiz "echo\n"
-    _F: .asciiz "Foxtrot\n"
-    _f: .asciiz "foxtrot\n"
-    _G: .asciiz "Golf\n"
-    _g: .asciiz "golf\n"
-    _H: .asciiz "Hotel\n"
-    _h: .asciiz "hotel\n"
-    _I: .asciiz "India\n"
-    _i: .asciiz "india\n"
-    _J: .asciiz "Juliet\n"
-    _j: .asciiz "juliet\n"
-    _K: .asciiz "Kilo\n"
-    _k: .asciiz "kilo\n"
-    _L: .asciiz "Lima\n"
-    _l: .asciiz "lima\n"
-    _M: .asciiz "Mary\n"
-    _m: .asciiz "mary\n"
-    _N: .asciiz "November\n"
-    _n: .asciiz "november\n"
-    _O: .asciiz "Oscar\n"
-    _o: .asciiz "oscar\n"
-    _P: .asciiz "Paper\n"
-    _p: .asciiz "paper\n"
-    _Q: .asciiz "Quebec\n"
-    _q: .asciiz "quebec\n"
-    _R: .asciiz "Research\n"
-    _r: .asciiz "research\n"
-    _S: .asciiz "Sierra\n"
-    _s: .asciiz "sierra\n"
-    _T: .asciiz "Tango\n"
-    _t: .asciiz "tango\n"
-    _U: .asciiz "Uniform\n"
-    _u: .asciiz "uniform\n"
-    _V: .asciiz "Victor\n"
-    _v: .asciiz "victor\n"
-    _W: .asciiz "Whisky\n"
-    _w: .asciiz "whisky\n"
-    _X: .asciiz "X-ray\n"
-    _x: .asciiz "x-ray\n"
-    _Y: .asciiz "Yankee\n"
-    _y: .asciiz "yankee\n"
-    _Z: .asciiz "Zulu\n"
-    _z: .asciiz "zulu\n"
-    _1: .asciiz "First\n"
-    _2: .asciiz "Second\n"
-    _3: .asciiz "Third\n"
-    _4: .asciiz "Fourth\n"
-    _5: .asciiz "Fifth\n"
-    _6: .asciiz "Sixth\n"
-    _7: .asciiz "Seventh\n"
-    _8: .asciiz "Eighth\n"
-    _9: .asciiz "Ninth\n"
-    _0: .asciiz "zero\n"
+string_table: .asciiz
+    _A: "Alpha\n",
+    _a: "alpha\n",
+    _B: "Bravo\n",
+    _b: "bravo\n",
+    _C: "China\n",
+    _c: "china\n",
+    _D: "Delta\n",
+    _d: "delta\n",
+    _E: "Echo\n",
+    _e: "echo\n",
+    _F: "Foxtrot\n",
+    _f: "foxtrot\n",
+    _G: "Golf\n",
+    _g: "golf\n",
+    _H: "Hotel\n",
+    _h: "hotel\n",
+    _I: "India\n",
+    _i: "india\n",
+    _J: "Juliet\n",
+    _j: "juliet\n",
+    _K: "Kilo\n",
+    _k: "kilo\n",
+    _L: "Lima\n",
+    _l: "lima\n",
+    _M: "Mary\n",
+    _m: "mary\n",
+    _N: "November\n",
+    _n: "november\n",
+    _O: "Oscar\n",
+    _o: "oscar\n",
+    _P: "Paper\n",
+    _p: "paper\n",
+    _Q: "Quebec\n",
+    _q: "quebec\n",
+    _R: "Research\n",
+    _r: "research\n",
+    _S: "Sierra\n",
+    _s: "sierra\n",
+    _T: "Tango\n",
+    _t: "tango\n",
+    _U: "Uniform\n",
+    _u: "uniform\n",
+    _V: "Victor\n",
+    _v: "victor\n",
+    _W: "Whisky\n",
+    _w: "whisky\n",
+    _X: "X-ray\n",
+    _x: "x-ray\n",
+    _Y: "Yankee\n",
+    _y: "yankee\n",
+    _Z: "Zulu\n",
+    _z: "zulu\n",
+    _1: "First\n",
+    _2: "Second\n",
+    _3: "Third\n",
+    _4: "Fourth\n",
+    _5: "Fifth\n",
+    _6: "Sixth\n",
+    _7: "Seventh\n",
+    _8: "Eighth\n",
+    _9: "Ninth\n",
+    _0: "zero\n"
 
     table:  
-        .word   _A
-        .word   _a
-        .word   _B
-        .word   _b
-        .word   _C
-        .word   _c
-        .word   _D
-        .word   _d
-        .word   _E
-        .word   _e
-        .word   _F
-        .word   _f
-        .word   _G
-        .word   _g
-        .word   _H
-        .word   _h
-        .word   _I
-        .word   _i
-        .word   _J
-        .word   _j
-        .word   _K
-        .word   _k
-        .word   _L
-        .word   _l
-        .word   _M
-        .word   _m
-        .word   _N
-        .word   _n
-        .word   _O
-        .word   _o
-        .word   _P
-        .word   _p
-        .word   _Q
-        .word   _q
-        .word   _R
-        .word   _r
-        .word   _S
-        .word   _s
-        .word   _T
-        .word   _t
-        .word   _U
-        .word   _u
-        .word   _V
-        .word   _v
-        .word   _W
-        .word   _w
-        .word   _X
-        .word   _x
-        .word   _Y
-        .word   _y
-        .word   _Z
-        .word   _z
-        .word   _1
-        .word   _2
-        .word   _3
-        .word   _4
-        .word   _5
-        .word   _6
-        .word   _7
-        .word   _8
-        .word   _9
-        .word   _0
-        .word   0
+        .word       _A,   _a,   _B,   _b,   _C,   _c,   _D,   _d,   _E,   _e,   _F,   _f,   _G,   _g,   _H,   _h,   _I,   _i,   _J,   _j,   _K,   _k,   _L,   _l,   _M,   _m,   _N,   _n,   _O,   _o,   _P,   _p,   _Q,   _q,   _R,   _r,   _S,   _s,   _T,   _t,   _U,   _u,   _V,   _v,   _W,   _w,   _X,   _x,   _Y,   _y,   _Z,   _z,   _1,   _2,   _3,   _4,   _5,   _6,   _7,   _8,   _9,   _0,   0
         
     
 .text 
@@ -165,6 +104,7 @@ main:
     # $t0:end    $t1:str arr index  $t2:input char              $t3:length     
     # $t4:(Temp) step               $t5:address of string[1]    $t6:string[i]
     # $s0:string    $s1:the char of string
+
     lb   $t0, end
     la   $s0, string
     
